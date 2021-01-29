cb6d404cae409c389a6c2a7a291d24015ade87d4
Jasper <you@example.com>
2021-01-29 23:56:06 +0800

    added application startup script

---
+++ autostart.sh
@@ -0,0 +1,42 @@
+#! /bin/sh 
+# start Firefox
+
+firefox &
+
+#Start LibreOffice
+
+soffice &
+
+
+#Start Dropbox
+
+urxvt &
+
+urxvt -e htop &
+
+urxvt -e ncmpcpp -s search_engine &
+
+
+urxvt -e ncmpcpp -s playlist &
+
+
+urxvt -e ncmpcpp -s browser &
+
+
+urxvt -e vis &
+
+dropbox &
+
+onedrivemount.sh & 
+
+#vmware &
+
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+
+#VirtualBox &
+
+#spotify &
+
+
+steam-runtime &
+
