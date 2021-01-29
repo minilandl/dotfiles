5f5407db9cef5ffae2bd56fbaa78c9991365fc54
Jasper <you@example.com>
2021-01-29 23:27:13 +0800

    added emulationstation extra files

---
+++ .emulationstation/scrapeauto.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+rm -rf $HOME/.emulationstation/gamelists/*/gamelist.xml
+Skyscraper -s Screenscraper -p nes
+Skyscraper -p nes  
+Skyscraper -s Screenscraper -p dreamcast
+Skyscraper -p dreamcast  
+Skyscraper -s Screenscraper -p nds
+Skyscraper -p nds  
+Skyscraper -s Screenscraper -p gbc
+Skyscraper -p gbc
+Skyscraper -s Screenscraper -p gc
+Skyscraper -p gc
+Skyscraper -s Screenscraper -p gba
+Skyscraper -p gba  
+Skyscraper -s Screenscraper -p genesis
+Skyscraper -p genesis
+Skyscraper -s Screenscraper -p n64
+Skyscraper -p n64
+Skyscraper -s Screenscraper -p neogeo
+Skyscraper -p neogeo
+Skyscraper -s Screenscraper -p nes
+Skyscraper -p nes
+Skyscraper -s Screenscraper -p ps2
+Skyscraper -p ps2
+Skyscraper -s Screenscraper -p psp
+Skyscraper -p psp
+Skyscraper -s Screenscraper -p segacd
+Skyscraper -p segacd
+Skyscraper -s Screenscraper -p wii
+Skyscraper -p wii
+Skyscraper -s Screenscraper -p wiiu
+Skyscraper -p wiiu
+Skyscraper -p snes
+Skyscraper -s Screenscraper -p snes
+Skyscraper -s Screenscraper -p psx
+Skyscraper -p psx 
