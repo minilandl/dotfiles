6ef253cea30650bcb910466f5d9030c933ad5929
minilandl <47686364+minilandl@users.noreply.github.com>
2021-01-30 21:33:50 +0800

    Delete .cache/wal directory

--- .cache/wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#ddd2de", "#3b1b22" },
-	[SchemeSel] = { "#ddd2de", "#6C5DA4" },
-	[SchemeOut] = { "#ddd2de", "#BC9FB7" },
-};
--- .cache/wal/colors-wal-tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "#3b1b22";
-static const char* selfgcolor   = "#ddd2de";
-static const char* normbgcolor  = "#4E76C5";
-static const char* normfgcolor  = "#ddd2de";
-static const char* urgbgcolor   = "#6C5DA4";
-static const char* urgfgcolor   = "#ddd2de";
