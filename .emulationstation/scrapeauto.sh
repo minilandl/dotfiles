135bb417b9ac323537f44d15f657fb66133cdc81
minilandl <jasper.greenhill@gmail.com>
2021-03-10 01:57:21 +0800

    updated scrape script

--- .emulationstation/scrapeauto.sh
+++ .emulationstation/scrapeauto.sh
@@ -1,36 +1,54 @@
 #!/bin/sh
+
+i=0
 rm -rf $HOME/.emulationstation/gamelists/*/gamelist.xml
-Skyscraper -s Screenscraper -p nes
-Skyscraper -p nes  
-Skyscraper -s Screenscraper -p dreamcast
-Skyscraper -p dreamcast  
-Skyscraper -s Screenscraper -p nds
-Skyscraper -p nds  
-Skyscraper -s Screenscraper -p gbc
-Skyscraper -p gbc
-Skyscraper -s Screenscraper -p gc
-Skyscraper -p gc
-Skyscraper -s Screenscraper -p gba
-Skyscraper -p gba  
-Skyscraper -s Screenscraper -p genesis
-Skyscraper -p genesis
-Skyscraper -s Screenscraper -p n64
-Skyscraper -p n64
-Skyscraper -s Screenscraper -p neogeo
-Skyscraper -p neogeo
-Skyscraper -s Screenscraper -p nes
+
+if i=0
+then
+Skyscraper -s sreenscraper -p nes
+Skyscraper -s screenscraper -p dreamcast
+Skyscraper -s screenscraper -p nds
+Skyscraper -s screenscraper -p gbc
+Skyscraper -s screenscraper -p gc
+Skyscraper -s screenscraper -p gba
+Skyscraper -s screenscraper -p megadrive
+Skyscraper -s screenscraper -p n64
+Skyscraper -s screenscraper -p neogeo
+Skyscraper -s screenscraper -p nes
+Skyscraper -s screenscraper -p ps2
+Skyscraper -s screenscraper -p psp
+Skyscraper -s screenscraper -p segacd
+Skyscraper -s screenscraper -p wii
+Skyscraper -s screenscraper -p wiiu
+Skyscraper -s screenscraper -p snes
+Skyscraper -s screenscraper -p psx
+Skyscraper -s screenscraper -p fds
+Skyscraper -s screenscraper -p gb
+i=$((i+1)) 
+echo $i
+elif i=1
+then
+Skyscraper -p fds
 Skyscraper -p nes
-Skyscraper -s Screenscraper -p ps2
+Skyscraper -p nds  
+Skyscraper -p gbc
+Skyscraper -p gc
+Skyscraper -p gba  
+Skyscraper -p megadrive
+Skyscraper -p neogeo
 Skyscraper -p ps2
-Skyscraper -s Screenscraper -p psp
 Skyscraper -p psp
-Skyscraper -s Screenscraper -p segacd
-Skyscraper -p segacd
-Skyscraper -s Screenscraper -p wii
-Skyscraper -p wii
-Skyscraper -s Screenscraper -p wiiu
-Skyscraper -p wiiu
-Skyscraper -p snes
-Skyscraper -s Screenscraper -p snes
-Skyscraper -s Screenscraper -p psx
+Skyscraper -p dreamcast 
+Skyscraper -p n64
 Skyscraper -p psx 
+Skyscraper -p nds
+Skyscraper -p gbc
+Skyscraper -p gb
+Skyscraper -p segacd
+Skyscraper -p snes
+Skyscraper -p gc
+Skyscraper -p wiiu
+Skyscraper -p wii
+Skyscraper -p gba  
+else echo "all done"
+fi
