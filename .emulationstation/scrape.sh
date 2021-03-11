f65bc847a4fa331551b01077adbba25e485340bd
minilandl <jasper.greenhill@gmail.com>
2021-03-11 17:39:06 +0800

    added scrape script

---
+++ .emulationstation/scrape.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+rm -rf $HOME/.emulationstation/gamelists/*/gamelist.xml
+Skyscraper -s sreenscraper -p nes 
+Skyscraper -s screenscraper -p dreamcast
+Skyscraper -s screenscraper -p nds 
+Skyscraper -s screenscraper -p gbc 
+Skyscraper -s screenscraper -p gc 
+Skyscraper -s screenscraper -p gba 
+Skyscraper -s screenscraper -p megadrive 
+Skyscraper -s screenscraper -p n64 
+Skyscraper -s screenscraper -p neogeo 
+Skyscraper -s screenscraper -p nes 
+Skyscraper -s screenscraper -p ps2 
+Skyscraper -s screenscraper -p psp 
+Skyscraper -s screenscraper -p segacd 
+Skyscraper -s screenscraper -p wii 
+Skyscraper -s screenscraper -p wiiu 
+Skyscraper -s screenscraper -p snes 
+Skyscraper -s screenscraper -p psx 
+Skyscraper -s screenscraper -p fds 
+Skyscraper -s screenscraper -p gb 
+Skyscraper -s thegamesdb -p nes --onlymissing
+Skyscraper -s thegamesdb  -p dreamcast --onlymissing
+Skyscraper -s thegamesdb -p nds --onlymissing
+Skyscraper -s thegamesdb -p gbc --onlymissing
+Skyscraper -s thegamesdb -p gc --onlymissing
+Skyscraper -s thegamesdb -p gba --onlymissing
+Skyscraper -s thegamesdb -p megadrive --onlymissing
+Skyscraper -s thegamesdb -p n64 --onlymissing
+Skyscraper -s thegamesdb -p neogeo --onlymissing
+Skyscraper -s thegamesdb -p nes --onlymissing
+Skyscraper -s thegamesdb -p ps2 --onlymissing
+Skyscraper -s thegamesdb -p psp --onlymissing
+Skyscraper -s thegamesdb -p segacd --onlymissing
+Skyscraper -s thegamesdb -p wii --onlymissing
+Skyscraper -s thegamesdb -p wiiu --onlymissing
+Skyscraper -s thegamesdb -p snes --onlymissing
+Skyscraper -s thegamesdb -p psx --onlymissing
+Skyscraper -s thegamesdb -p fds --onlymissing
+Skyscraper -s thegamesdb -p gb --onlymissing
+
+
+
+
+
+
+Skyscraper -p fds                        
+Skyscraper -p nes                        
+Skyscraper -p nds                        
+Skyscraper -p gbc                      
+Skyscraper -p gc                       
+Skyscraper -p gba                      
+Skyscraper -p megadrive               
+Skyscraper -p neogeo                  
+Skyscraper -p ps2    
+Skyscraper -p psp    
+Skyscraper -p dreamcast     
+Skyscraper -p n64    
+Skyscraper -p psx     
+Skyscraper -p nds    
+Skyscraper -p gbc    
+Skyscraper -p gb    
+Skyscraper -p segacd    
+Skyscraper -p snes    
+Skyscraper -p gc    
+Skyscraper -p wiiu    
+Skyscraper -p wii    
+Skyscraper -p gba      
+
