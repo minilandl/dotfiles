eefd16f3565c4413f97f11b193128bff51d99da7
minilandl <jasper.greenhll@gmail.com>
2022-11-11 19:13:41 +0800

    fixed ea desktop

--- .bin/originfix.sh
+++ .bin/originfix.sh
@@ -2,3 +2,6 @@
 wineprefix=/mnt/Games/SteamLibrarynative/steamapps/compatdata/wine
 
 sudo rsync -avP $wineprefix/drive_c/ProgramData/Origin/SelfUpdate/Staged/ $wineprefix/drive_c/Program\ Files\ \(x86\)/Origin/
+
+sudo sync -avP  "$wineprefix/drive_c/Program Files/Electronic Arts/EA Desktop/StagedEADesktop/EA Desktop/"  "$wineprefix/drive_c/Program Files/Electronic Arts/EA Desktop/EA Desktop/"
+
